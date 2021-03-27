b9cb756162b9c18d9fe68bbac55e16295ff34fc0
eastrd <yjia285@student.monash.edu>
2021-03-28 09:28:47 +1100

    Add basic auth + TLS support with environment variables

--- main.go
+++ main.go
@@ -3,15 +3,58 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+var (
+	// BASICUSER : Basic Auth Username
+	BASICUSER string
+	// BASICPASS : Basic Auth Password
+	BASICPASS string
+	// CERTFILE : Path to cert file
+	CERTFILE string
+	// KEYFILE : Path to key file
+	KEYFILE string
+)
+
+func init() {
+	// Check environment variables are given
+	BASICUSER = os.Getenv("BASIC_USER")
+	BASICPASS = os.Getenv("BASIC_PASS")
+	CERTFILE = os.Getenv("CERT")
+	KEYFILE = os.Getenv("KEY")
+
+	if len(BASICUSER) == 0 || len(BASICPASS) == 0 {
+		log.Panic("Basic Username/Password is empty")
+	}
+	if len(CERTFILE) == 0 || len(KEYFILE) == 0 {
+		log.Panic("Cert/Key is empty")
+	}
+}
+
 func main() {
-	http.HandleFunc("/getwordfreq", handleGetWordFrequency)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/getwordfreq", handleGetWordFrequency)
 
 	log.Println("server started at port 8080")
-	// err := http.ListenAndServeTLS(":8090", "../certs/cert.pem", "../certs/key.pem", nil)
-	err := http.ListenAndServe(":8080", nil)
+
+	err := http.ListenAndServeTLS(":8080", CERTFILE, KEYFILE, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check Basic Auth before /getwordfreq
+		u, p, ok := r.BasicAuth()
+		if !ok {
+			log.Println("error parsing basic auth")
+			w.WriteHeader(401)
+			return
+		}
+		if u != BASICUSER || p != BASICPASS {
+			log.Println("basic auth login failed")
+			w.WriteHeader(401)
+			return
+		}
+		mux.ServeHTTP(w, r)
+	}))
+
 	if err != nil {
-		panic(err)
+		log.Fatalf("error starting server %s", err.Error())
 	}
 }
